1ba41ea008f46298e587b8ccdd0c72dc7cb2463b
unknown <folarinfadeyi@gmail.com>
2023-02-07 15:06:41 -0800

    hello

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,9 +6,6 @@ const Home = () => {
             <div>Home Page</div>
             <br />
             <Outlet />
-            {/* <div><Link to='/about' className="btn">About Us</Link></div>
-            <br />
-            <div><Link to='/products' className='btn'>Products</Link></div> */}
         </section>
     )
 }
