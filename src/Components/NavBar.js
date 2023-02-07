dc26031973be58bf0a1f039863f3a97baecb5805
unknown <folarinfadeyi@gmail.com>
2023-02-07 15:10:06 -0800

    oh well...

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -2,7 +2,12 @@ import { Link } from "react-router-dom"
 
 const NavBar = () => {
   return (
-    <nav className="navbar">Navbar</nav>
+    <nav className="navbar">
+        <Link to='/' >Home</Link>
+        <Link to='/about' >About Us</Link>
+        <Link to='/products' >Products</Link>
+        <Link to='*' >Error</Link>
+    </nav>
   )
 }
 
