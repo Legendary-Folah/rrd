c7a7623c8faf652031a9019dbcc8663a372aeb92
unknown <folarinfadeyi@gmail.com>
2023-02-07 15:11:28 -0800

    oh well...

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,11 @@
 import { Link, Outlet } from "react-router-dom"
+import NavBar from "./NavBar"
 
 const Home = () => {
     return (
         <section className="section">
             <div>Home Page</div>
-            <br />
+            {/* <br /> */}
             <Outlet />
         </section>
     )
