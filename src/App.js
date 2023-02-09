b7e379fb673b015847d5684ffa77bf0d699e9fa9
unknown <folarinfadeyi@gmail.com>
2023-02-09 14:34:59 -0800

    oh well...

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Products from './Components/Products';
 import Home from './Components/Home';
 import Error from './Components/Error';
 import SharedLayout from './Components/SharedLayout'
+import SingleProduct from './Components/SingleProduct';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
             <Route index element={<Home />} />
             <Route path='about' element={<About />}/>
             <Route path='products' element={<Products />}/>
+            <Route path='products/:productID' element={<SingleProduct />} />
             <Route path='*' element={<Error />}/>
             </Route>
         </Routes>
