ee1a9ad009301a08bf90d79b5afc86412d970484
unknown <folafad15@gmail.com>
2023-02-09 15:57:46 -0800

    more and more changes as we progress

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -4,7 +4,14 @@ import products from "../Data"
 const Products = () => {
     return (
         <section className="section">
-            <div>Products Page</div>
+            <h2>Products</h2>
+            <div className="products">
+                {products.map((product) => {
+                    return <article key={product.id}>
+                        {product.name}
+                    </article>
+                })}
+            </div>
             <Link to='/' className="btn">Home</Link>
         </section>
     )
