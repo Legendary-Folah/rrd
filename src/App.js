e4efc11fe72b2f48bc79cd109d9bec3f99757d23
unknown <folarinfadeyi@gmail.com>
2023-02-09 14:01:38 -0800

    oh well...

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import About from './Components/About';
 import Products from './Components/Products';
 import Home from './Components/Home';
 import Error from './Components/Error';
+import SharedLayout from './Components/SharedLayout'
 
 function App() {
   return (
     <BrowserRouter>
         <Routes>
-            <Route path='/' element={<Home />}>
+            <Route path='/' element={<SharedLayout />}>
+            <Route index element={<Home />} />
             <Route path='about' element={<About />}/>
             <Route path='products' element={<Products />}/>
             <Route path='*' element={<Error />}/>
