ba81106ae6dffd11e62a83c4fd5b88f343624459
unknown <folarinfadeyi@gmail.com>
2023-02-09 15:11:47 -0800

    Hello there

--- src/Components/SingleProduct.js
+++ src/Components/SingleProduct.js
@@ -1,9 +1,7 @@
 import { Link, useParams } from 'react-router-dom'
 
 const SingleProduct = () => {
-
-
-    
+    console.log(useParams())
     return (
         <section className="section product">
             <h3>Single Product page</h3>
