3ac3664962014ea3a31791b830edb0f43c9a5f10
unknown <folarinfadeyi@gmail.com>
2023-02-09 14:33:13 -0800

    oh well...

---
+++ src/Components/SingleProduct.js
@@ -0,0 +1,8 @@
+const SingleProduct = () => {
+    return (
+        <section className="section product">
+            <h3>SIngle Product page</h3>
+        </section>
+    )
+}
+export default SingleProduct
