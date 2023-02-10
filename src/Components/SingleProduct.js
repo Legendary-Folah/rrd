7a91872297c7a13ff7d5ed3d903688621be4dc4b
unknown <folafad15@gmail.com>
2023-02-09 16:12:26 -0800

    more and more changes as we progress

--- src/Components/SingleProduct.js
+++ src/Components/SingleProduct.js
@@ -1,8 +1,12 @@
 import { Link, useParams } from "react-router-dom"
+import products from "../Data"
 
 const SingleProduct = () => {
     
     const { productID } = useParams()
+    const product = products.find((products) => product.id === 
+    productID
+    )
     return (
         <section className="section product">
             <h3>{productID}</h3>
