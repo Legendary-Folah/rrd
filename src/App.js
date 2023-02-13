bded19996546c527f2b9eab4011789f75eb4dc2c
unknown <folarinfadeyi@gmail.com>
2023-02-13 12:46:46 -0800

    more and more changes as we progress

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ function App() {
             <Route path='about' element={<About />}/>
             <Route path='products' element={<Products />}/>
             <Route path='products/:productID' element={<SingleProduct />} />
+            <Route path='login' element={<Login />} />
             <Route path='*' element={<Error />}/>
             </Route>
         </Routes>
