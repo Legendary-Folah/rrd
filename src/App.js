ccd6443a7df010431e9011608143ac15eff48b40
unknown <folarinfadeyi@gmail.com>
2023-02-13 12:50:07 -0800

    more and more changes as we progress

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ function App() {
             <Route path='products' element={<Products />}/>
             <Route path='products/:productID' element={<SingleProduct />} />
             <Route path='login' element={<Login  setUser={setUser}/>} />
+            <Route path='dashboard' element={<Dashboard  user={user}/>} />
             <Route path='*' element={<Error />}/>
             </Route>
         </Routes>
