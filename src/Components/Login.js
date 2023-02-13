0b2f6c4d0d3de84fa1b58ab776f4f463f54c8f2b
unknown <folarinfadeyi@gmail.com>
2023-02-13 14:52:12 -0800

    more and more changes as we progress

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -7,6 +7,17 @@ const Login = () => {
         <section className='section'>
             <form className='form'>
                 <h3>Login</h3>
+                <div className='form-row'>
+                    <label htmlFor='name' className='form-label'>
+                        Name
+                    </label>
+                    <input 
+                        type='text'
+                        className='form-input'
+                        value={name}
+                        onChange={() => {}}
+                    />
+                </div>
             </form>
         </section>
     )
