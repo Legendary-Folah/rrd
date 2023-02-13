70e390acc7b5e0f6367f8e8c43de0f9ff9c3e5d3
unknown <folarinfadeyi@gmail.com>
2023-02-13 14:03:55 -0800

    more and more changes as we progress

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 
 const Login = () => {
+    const [ name, setName ] = useState('')
+    const [ email, setEmail ] = useState('')
     return (
         <div>Log In</div>
     )
