b8c11bfbaf1bc487dce63ec83f9a1164289ad436
unknown <folarinfadeyi@gmail.com>
2023-02-14 10:32:42 -0800

    more and more changes as we progress

--- src/Components/ProtectedRoute.js
+++ src/Components/ProtectedRoute.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const ProtectedRoute = () => {
-  return (
-    <div>ProtectedRoute</div>
-  )
-}
+const ProtectedRoute = ({ children, user }) => {
+ if(!user) {
+    return <Navigate to='/' />
+ } 
+ return children;
+};
 
-export default ProtectedRoute
+export default ProtectedRoute;
