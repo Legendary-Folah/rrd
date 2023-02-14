8a5533a0f1dc7da34315f08c81e1b51b3f261475
unknown <folarinfadeyi@gmail.com>
2023-02-14 11:58:58 -0800

    One more ;

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom'
 
 const Login = ({ setUser }) => {
     const [ name, setName ] = useState('')
-    const [ email, setEmail ] = useState('')
+    const [ email, setEmail ] = useState('')  
 
     const navigate = useNavigate();
 
@@ -12,7 +12,7 @@ const Login = ({ setUser }) => {
         // console.log(name, email);
         if (!name || !email) return;
         setUser({ name:name, email:email });
-        navigate('/dashboard')
+        navigate('/dashboard');
     }
 
     return (
